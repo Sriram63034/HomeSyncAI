745a0f05f383ced445e49a0e9d1e3efd21c75752
devana sriram <devanasriram@devanas-MacBook-Air.local>
2026-03-19 02:15:30 +0530

    productionupdate

--- Backend/homesync_backend/settings.py
+++ Backend/homesync_backend/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-pbi#s6837g@x&e04j&7j-3$aak45*3h2pu%kzgn!yj*=ms!5%k"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
